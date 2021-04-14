aa08c56007d5fab9c7e1952a5e7c668855160011
domynique <dshelbyv@gmail.com>
2021-04-14 04:01:41 -0400

    new

---
+++ app.js
@@ -0,0 +1,30 @@
+
+function squareFunction(param1){
+    answer= param1* parama1;
+    console.log (answer);
+
+}
+
+function addTwo(x,y){
+    answer = x+y;
+    console.log(answer);
+}
+addTwo(2,4)
+
+function subTwo(x,y){
+    answer = x-y;
+    console.log(answer);
+}
+subTwo(15,10)
+
+function functionWithArgs(x,y) {
+    answer = (x*y)
+    console.log(answer)
+}
+functionWithArgs(2,4)
+
+function functionWithArgs (x,y){
+    answer = (x/y)
+    console.log(answer)
+}
+    functionWithArgs(8,4)
